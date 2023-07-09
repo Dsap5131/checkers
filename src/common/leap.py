797e493396ca9651a2b95f6268d92c0e4ee8122f
Dsapi5131 <dsap@tutamail.com>
2023-07-09 04:53:32 -0400

    Override __eq__ for Leap

--- src/common/leap.py
+++ src/common/leap.py
@@ -5,6 +5,7 @@ class Leap():
     A Leap represents a singular move a GamePiece makes.
     '''
 
+
     def __init__(self, 
                  start_position: Position, 
                  end_position: Position) -> None:
@@ -22,6 +23,7 @@ class Leap():
 
         return self.__start_position
     
+
     def get_end_position(self) -> Position:
         '''
         Get the end position of the leap.
@@ -30,3 +32,16 @@ class Leap():
         '''
 
         return self.__end_position
+    
+
+    def __eq__(self, obj) -> bool:
+        '''
+        Leaps are equal if they have equal starting_positions and 
+        ending_positions.
+
+        @returns: bool
+        '''
+
+        return (isinstance(obj, Leap) and
+                obj.get_start_position() == self.get_start_position() and
+                obj.get_end_position() == self.get_end_position())
--- testing/common/test_leap.py
+++ testing/common/test_leap.py
@@ -24,3 +24,27 @@ def test_get_end_position() -> None:
 
     assert leap.get_end_position() == end_position, \
         "Leap.get_end_position() not working."
+
+
+def test_eq() -> None:
+    start_position_one = Position(0,0)
+    start_position_two = Position(0,0)
+    start_position_three = Position(1,1)
+
+    end_position_one = Position(2,2)
+    end_position_two = Position(2,2)
+    end_position_three = Position(3,3)
+
+    leap_one = Leap(start_position_one, end_position_one)
+    leap_two = Leap(start_position_two, end_position_two)
+    leap_three = Leap(start_position_two, end_position_three)
+    leap_four = Leap(start_position_three, end_position_two)
+
+    assert leap_one == leap_two, \
+        "Leap == Leap not working correctly."
+    assert (leap_one == leap_three) == False, \
+        "Leap == Leap not working correctly."
+    assert (leap_one == leap_four) == False, \
+        "Leap == Leap not working correctly."
+    assert (leap_one == 3) == False, \
+        "Leap == Leap not working correctly."
