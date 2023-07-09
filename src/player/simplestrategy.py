26d6b82216020ea8ba19ea08bf3a3f3c202dba38
Dsapi5131 <dsap@tutamail.com>
2023-07-09 05:07:00 -0400

    Fix simplestrategy bug

--- src/player/simplestrategy.py
+++ src/player/simplestrategy.py
@@ -4,7 +4,7 @@ from src.common.playergamestate import PlayerGameState
 from src.common.move import Move
 from src.common.leap import Leap
 from src.common.position import Position
-from src.common.rules import Rules
+
 
 class SimpleStrategy():
     '''
@@ -12,7 +12,7 @@ class SimpleStrategy():
     top left position.
     '''
 
-    def make_move(self, playergamestate: PlayerGameState, rules: Rules) -> Move:
+    def make_move(self, playergamestate: PlayerGameState) -> Move:
         return Move(deque([Leap(Position(0,0), Position(0,0))]))
 
     
--- testing/player/test_simplestrategy.py
+++ testing/player/test_simplestrategy.py
@@ -23,8 +23,9 @@ def test_make_move() -> None:
                   [GamePiece.BLANK, GamePiece.BLANK]]
     board = Board(row_size, column_size, board_list)
     rules = RulesStandard()
+    playergamestate = PlayerGameState(board, rules)
 
     expected_move = Move(deque([Leap(Position(0,0), Position(0,0))]))
 
-    assert strategy.make_move(board, rules) == expected_move, \
+    assert strategy.make_move(playergamestate) == expected_move, \
         "SimpleStrategy.make_move(Board, Rules) not working as expected."
