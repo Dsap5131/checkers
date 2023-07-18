33aea260cd8eb5408a38634560b9878021d0c0c8
Dsapi5131 <dsap@tutamail.com>
2023-07-17 21:52:01 -0400

    Improve basicstartegy testing

--- testing/player/test_basicstrategy.py
+++ testing/player/test_basicstrategy.py
@@ -19,23 +19,57 @@ def test_constructor() -> None:
 def test_make_move() -> None:
     strategy = BasicStrategy()
 
+    # Test basic red move
     blank_piece = GamePiece(Piece.BLANK, False)
     red_piece = GamePiece(Piece.RED, False)
     black_piece = GamePiece(Piece.BLACK, False)
     row_size=3
     column_size=3
-    board_list=[[blank_piece, blank_piece, black_piece],
+    board_list_1=[[blank_piece, blank_piece, black_piece],
                 [blank_piece, red_piece, blank_piece],
                 [blank_piece, blank_piece, red_piece]]
-    board = Board(row_size, column_size, board_list)
+    board_1 = Board(row_size, column_size, board_list_1)
 
     rules = RulesStandard()
-    player = LocalPlayer(Piece.RED, strategy)
-    playergamestate = PlayerGameState(board, rules, player)
+    player_red = LocalPlayer(Piece.RED, strategy)
+    playergamestate_2 = PlayerGameState(board_1, rules, player_red)
     expected_move = Move(deque([Leap(Position(1,1), Position(0,0))]))
-    actual_move = strategy.make_move(playergamestate)
+    actual_move = strategy.make_move(playergamestate_2)
 
     assert actual_move == expected_move, \
         "BasicStrategy.make_move() not working correctly."
+    
+    # Test basic black move
+    player_black = LocalPlayer(Piece.BLACK, strategy)
+    board_list_2 = [[blank_piece, blank_piece, blank_piece],
+                    [blank_piece, black_piece, blank_piece],
+                    [blank_piece, blank_piece, blank_piece]]
+    board_2 = Board(row_size=3, column_size=3, board=board_list_2)
+    playergamestate_2 = PlayerGameState(board_2, rules, player_black)
+    actual_move = strategy.make_move(playergamestate_2)
+    expected_move = Move(deque([Leap(Position(1,1), Position(2,0))]))
+    assert actual_move == expected_move, \
+        "BasicStrategy.make_move() not working correctly."
+    
+    # Test prioritzed capture over regular leap
+    board_list_3 = [[blank_piece, blank_piece, blank_piece, blank_piece],
+                    [blank_piece, black_piece, blank_piece, blank_piece],
+                    [blank_piece, blank_piece, red_piece, blank_piece],
+                    [blank_piece, blank_piece, blank_piece, blank_piece]]
+    board_3 = Board(row_size=4, column_size=4, board=board_list_3)
+    playergamestate_3 = PlayerGameState(board_3, rules, player_black)
+    actual_move = strategy.make_move(playergamestate_3)
+    expected_move = Move(deque([Leap(Position(1,1), Position(3,3))]))
+    assert actual_move == expected_move, \
+        "BasicStrategy.make_move() not working correctly."
+    
+
+
+    
+
+
+    
+
+
     
 
