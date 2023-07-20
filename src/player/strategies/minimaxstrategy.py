ce68d8fd030b31387c5286aaab1c1fd8a55c7927
Dylan Sapienza <loldsap5131@gmail.com>
2023-07-20 15:33:19 +0000

    Start MiniMaxStrategy

--- src/player/strategies/minimaxstrategy.py
+++ src/player/strategies/minimaxstrategy.py
@@ -1,5 +1,9 @@
 from src.player.strategies.strategy import Strategy
 from src.common.move import Move
+from src.common.playergamestate import PlayerGameState
+from src.common.rules import Rules
+from src.common.player import Player
+from src.common.board import Board
 
 class MiniMaxStrategy(Strategy):
     """
@@ -7,6 +11,27 @@ class MiniMaxStrategy(Strategy):
     next best move.
     """
 
-    def make_move(playergamestate: "PlayerGameState") -> Move:
-        ...
+    def make_move(self, playergamestate: PlayerGameState) -> Move:
+        '''
+        Make a move based on the given playergamestate.
+
+        Given playergamestate should never be in a gameover state. Thus a valid
+        move should exist.
+
+        @param: playergamestate: PlayerGameState
+
+        @returns: Move
+        '''
+        
+        rules = playergamestate.get_rules()
+        board = playergamestate.get_board()
+        player = playergamestate.get_player()
+
+        # Get Valid Moves
+        #valid_moves = self.__get_valid_moves(rules, board, player)
+        # if a valid move leads to a winning gamestate, store that move.
+        # Tiebreaker between winning moves. 
+            # Prioritize moving the piece to the top left most position
+        
+        
 
--- testing/player/strategies/README.md
+++ testing/player/strategies/README.md
@@ -7,4 +7,5 @@ Testing for the src/player/strategies
 |   ---            | ---   |
 | [test_strategy.py](./test_strategy.py) | Tests for [Strategy](../../src/player/strategy.py) |
 | [test_simplestrategy.py](./test_dumbstrategy.py) | Tests for [DumbStrategy](../../src/player/dumbstrategy.py) |
-| [test_basicstrategy.py](./test_basicstrategy.py) | Tests for [BasicStrategy](../../str/player/basicstrategy.py) |
+| [test_basicstrategy.py](./test_basicstrategy.py) | Tests for [BasicStrategy](../../src/player/basicstrategy.py) |
+| [test_checkers.py](./test_minimaxstrategy.py) | Tests for [MiniMaxStrategy](../../src/player/minimaxstrategy.py) |
