10164f7a82a9810838ce5ad6d339bb292c53fa70
Dylan Sapienza <loldsap5131@gmail.com>
2023-07-20 13:39:50 +0000

    Update leap to include information about captures

--- src/common/leap.py
+++ src/common/leap.py
@@ -1,17 +1,27 @@
+
+
 from src.common.position import Position
 
 class Leap():
     '''
     A Leap represents a singular move a GamePiece makes.
+    This includes the data about the movement of a piece and which pieces
+    it captures.
+
+    @params: start_position: Position
+    @params: end_position: Position
+    @params: capture_positions: List[Positions]: (Default) None
     '''
 
 
     def __init__(self, 
                  start_position: Position, 
-                 end_position: Position) -> None:
+                 end_position: Position,
+                 capture_positions: list[Position]=None) -> None:
         
         self.__start_position = start_position
         self.__end_position = end_position
+        self.__capture_positions = capture_positions
 
     
     def get_start_position(self) -> Position:
@@ -34,6 +44,16 @@ class Leap():
         return self.__end_position
     
 
+    def get_capture_positions(self) -> list[Position]:
+        '''
+        Get the positions captured by this leap.
+
+        @returns: List[Positions]
+        '''
+
+        return self.__capture_positions
+    
+
     def __eq__(self, obj) -> bool:
         '''
         Leaps are equal if they have equal starting_positions and 
@@ -44,4 +64,5 @@ class Leap():
 
         return (isinstance(obj, Leap) and
                 obj.get_start_position() == self.get_start_position() and
-                obj.get_end_position() == self.get_end_position())
+                obj.get_end_position() == self.get_end_position() and
+                obj.get_capture_positions() == self.get_capture_positions())
--- testing/common/test_leap.py
+++ testing/common/test_leap.py
@@ -24,6 +24,16 @@ def test_get_end_position() -> None:
 
     assert leap.get_end_position() == end_position, \
         "Leap.get_end_position() not working."
+    
+
+def test_get_captures() -> None:
+    start_position = Position(0,0)
+    end_position = Position(2,2)
+    capture_position = Position(1,1)
+    leap = Leap(start_position, end_position, [capture_position])
+
+    assert leap.get_capture_positions() == [Position(1,1)], \
+        "Leap.get_capture() not working correctly."
 
 
 def test_eq() -> None:
@@ -48,3 +58,47 @@ def test_eq() -> None:
         "Leap == Leap not working correctly."
     assert (leap_one == 3) == False, \
         "Leap == Leap not working correctly."
+    
+    # Test eq with captures
+    start_position_five = Position(0,0)
+    end_position_five = Position(2,2)
+    capture_positions_five = [Position(1,1)]
+    leap_five = Leap(start_position_five, 
+                     end_position_five, 
+                     capture_positions_five)
+
+    start_position_six = Position(0,0)
+    end_position_six = Position(2,2)
+    capture_positions_six = [Position(1,1)]
+    leap_six = Leap(start_position_six,
+                    end_position_six,
+                    capture_positions_six)
+    
+    start_position_seven = Position(0,0)
+    end_position_seven = Position(2,2)
+    capture_positions_seven = [Position(1,1), Position(3,3)]
+    leap_seven = Leap(start_position_seven,
+                      end_position_seven,
+                      capture_positions_seven)
+
+    start_position_eight = Position(0,0)
+    end_position_eight = Position(2,2)
+    capture_positions_eight = [Position(3,3)]
+    leap_eight = Leap(start_position_eight,
+                      end_position_eight,
+                      capture_positions_eight)
+    
+    start_position_nine = Position(0,0)
+    end_position_nine = Position(2,2)
+    leap_nine = Leap(start_position_nine, end_position_nine)
+    
+    assert leap_five == leap_six, \
+        "Leap.__eq__ not working correctly."
+    assert leap_five != leap_seven, \
+        "Leap.__eq__ not working correctly."
+    assert leap_five != leap_eight, \
+        "Leap.__eq__ not working correctly."
+    assert leap_five != leap_nine, \
+        "Leap.__eq__ not working correctly."
+
+
